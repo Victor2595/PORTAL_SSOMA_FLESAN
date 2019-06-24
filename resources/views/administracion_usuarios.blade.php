598c092c75633be6a19e6f852c9693a6a5e8b10c
Victor Flores <vflores@flesan.com.pe>
2019-06-24 16:34:26 -0500

    preprd

--- resources/views/administracion_usuarios.blade.php
+++ resources/views/administracion_usuarios.blade.php
@@ -272,7 +272,7 @@
             <div class="modal-dialog">
               <div class="modal-content">
                   <div class="modal-header" style="background-color: #d33724 !important;color: #ffffff">
-                    <button type="button" class="close" data-dismiss="modal" onclick="cancelar();" aria-label="Close">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                       <span aria-hidden="true">&times;</span></button>
                     <h4 class="modal-title">Registrar Usuarios</h4>
                   </div>
